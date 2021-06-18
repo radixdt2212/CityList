d9cf9ebcb29aa33a36a4c871c6e8018b392d5e60
radixdt2212 <85940721+radixdt2212@users.noreply.github.com>
2021-06-18 13:03:47 +0530

    First commit

---
+++ src/tourData.js
@@ -0,0 +1,37 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "New York",
+    img: "https://tutorials.rxtrainees.radixweb.net/wp-content/uploads/2020/12/logo_2500X1875-2048x1536.jpg",
+    name: "New york bridge tour",
+    info: "Hello there this is some info",
+  },
+  {
+    id: 2,
+    city: "Delhi",
+    img: "https://tutorials.rxtrainees.radixweb.net/wp-content/uploads/2020/12/logo_2500X1875-2048x1536.jpg",
+    name: "Delhi bridge tour",
+    info: "Hello there this is some info",
+  },
+  {
+    id: 3,
+    city: "New Delhi",
+    img: "https://tutorials.rxtrainees.radixweb.net/wp-content/uploads/2020/12/logo_2500X1875-2048x1536.jpg",
+    name: "New Delhi bridge tour",
+    info: "Hello there this is some info",
+  },
+  {
+    id: 4,
+    city: "Vadodara",
+    img: "https://tutorials.rxtrainees.radixweb.net/wp-content/uploads/2020/12/logo_2500X1875-2048x1536.jpg",
+    name: "Vadodara bridge tour",
+    info: "Hello there this is some info",
+  },
+  {
+    id: 5,
+    city: "Ahmedabad",
+    img: "https://tutorials.rxtrainees.radixweb.net/wp-content/uploads/2020/12/logo_2500X1875-2048x1536.jpg",
+    name: "Ahmedabad bridge tour",
+    info: "Hello there this is some info",
+  },
+];
